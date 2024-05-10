6def49aa027dac9681f60489eecebcb59b8990e1
Anastasiya lapatsyeva <a.p.lapatsueva@students.psu.by>
2024-05-10 16:49:22 +0300

    готовая форма

--- Project3/MyForm.h
+++ Project3/MyForm.h
@@ -47,6 +47,8 @@ namespace Project3 {
 	private: System::Windows::Forms::TextBox^ textBox3;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label6;
 	protected:
 
 	private:
@@ -62,9 +64,10 @@ namespace Project3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea3 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^ legend3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^ series3 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->label2 = (gcnew System::Windows::Forms::Label());
@@ -76,123 +79,162 @@ namespace Project3 {
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
 			this->label1->Location = System::Drawing::Point(13, 13);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(443, 20);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
+			this->label1->Text = L"Построение графика функции на интервале [xn;xk]";
 			// 
 			// chart1
 			// 
-			chartArea3->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea3);
-			legend3->Name = L"Legend1";
-			this->chart1->Legends->Add(legend3);
-			this->chart1->Location = System::Drawing::Point(16, 211);
+			this->chart1->BackColor = System::Drawing::Color::LightCyan;
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Enabled = false;
+			legend1->Name = L"график функции";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(8, 211);
 			this->chart1->Name = L"chart1";
-			series3->ChartArea = L"ChartArea1";
-			series3->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
-			series3->IsVisibleInLegend = false;
-			series3->Legend = L"Legend1";
-			series3->Name = L"Series1";
-			series3->XValueType = System::Windows::Forms::DataVisualization::Charting::ChartValueType::Double;
-			series3->YValueType = System::Windows::Forms::DataVisualization::Charting::ChartValueType::Double;
-			this->chart1->Series->Add(series3);
-			this->chart1->Size = System::Drawing::Size(469, 207);
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series1->IsVisibleInLegend = false;
+			series1->Legend = L"график функции";
+			series1->Name = L"График функции";
+			series1->XValueType = System::Windows::Forms::DataVisualization::Charting::ChartValueType::Double;
+			series1->YValueType = System::Windows::Forms::DataVisualization::Charting::ChartValueType::Double;
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(473, 254);
 			this->chart1->TabIndex = 1;
 			this->chart1->Text = L"chart1";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(343, 13);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(335, 67);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
+			this->label2->Size = System::Drawing::Size(104, 13);
 			this->label2->TabIndex = 2;
-			this->label2->Text = L"label2";
+			this->label2->Text = L"Введите данные";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(290, 52);
+			this->label3->Location = System::Drawing::Point(292, 106);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(35, 13);
+			this->label3->Size = System::Drawing::Size(67, 13);
 			this->label3->TabIndex = 3;
-			this->label3->Text = L"label3";
+			this->label3->Text = L"Введите XN";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(290, 97);
+			this->label4->Location = System::Drawing::Point(292, 151);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(35, 13);
+			this->label4->Size = System::Drawing::Size(66, 13);
 			this->label4->TabIndex = 4;
-			this->label4->Text = L"label4";
+			this->label4->Text = L"Введите XK";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(290, 138);
+			this->label5->Location = System::Drawing::Point(292, 192);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(35, 13);
+			this->label5->Size = System::Drawing::Size(67, 13);
 			this->label5->TabIndex = 5;
-			this->label5->Text = L"label5";
+			this->label5->Text = L"Введите XH";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(371, 51);
+			this->textBox1->Location = System::Drawing::Point(371, 105);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 6;
+			this->textBox1->Text = L"-1,78";
 			this->textBox1->Leave += gcnew System::EventHandler(this, &MyForm::textBox1_Leave);
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(371, 86);
+			this->textBox2->Location = System::Drawing::Point(371, 140);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 7;
+			this->textBox2->Text = L"11,99";
 			this->textBox2->Leave += gcnew System::EventHandler(this, &MyForm::textBox2_Leave);
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(371, 131);
+			this->textBox3->Location = System::Drawing::Point(371, 185);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(100, 20);
 			this->textBox3->TabIndex = 8;
+			this->textBox3->Text = L"0,5";
 			this->textBox3->Leave += gcnew System::EventHandler(this, &MyForm::textBox3_Leave);
 			// 
 			// button1
 			// 
+			this->button1->BackColor = System::Drawing::SystemColors::HotTrack;
 			this->button1->Location = System::Drawing::Point(16, 471);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(222, 30);
 			this->button1->TabIndex = 9;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Text = L"Нарисовать график";
+			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
+			this->button2->BackColor = System::Drawing::SystemColors::HotTrack;
 			this->button2->Location = System::Drawing::Point(295, 471);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(176, 30);
 			this->button2->TabIndex = 10;
-			this->button2->Text = L"button2";
-			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Text = L"Выход";
+			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(8, 67);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(278, 138);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 11;
+			this->pictureBox1->TabStop = false;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(53, 50);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(60, 13);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"Функция";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::GradientActiveCaption;
 			this->ClientSize = System::Drawing::Size(493, 551);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox3);
@@ -204,9 +246,11 @@ namespace Project3 {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->chart1);
 			this->Controls->Add(this->label1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedToolWindow;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Выполнил студент группы 23-КФ Лапацуева А. П.";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
